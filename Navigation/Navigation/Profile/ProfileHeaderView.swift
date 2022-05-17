ad9dd672c56f02c428c5b10a45b24daa1ad36301
PekyPirogi <v.k.martynov@yandex.ru>
2022-05-17 16:58:06 +0500

    All done, ready to pull

--- Navigation/Navigation/Profile/ProfileHeaderView.swift
+++ Navigation/Navigation/Profile/ProfileHeaderView.swift
@@ -8,13 +8,16 @@
 import Foundation
 import UIKit
 
-
 class ProfileHeaderView: UIView {
     
+    private var statusText: String
 
     override init(frame: CGRect) {
+        statusText = ""
         super.init(frame: frame)
-        addElements()
+        
+        [avatarView, headerView, statusView, statusButton, statusChange, headerBack].forEach{addSubview($0)}
+        
     }
     
     required init?(coder: NSCoder) {
@@ -42,8 +45,8 @@ class ProfileHeaderView: UIView {
         return header
     }()
     
-    private let statusView: UITextView = {
-        let status = UITextView(frame: CGRect(x: 150, y: 170, width: 300, height: 30))
+    private let statusView: UILabel = {
+        let status = UILabel(frame: CGRect(x: 150, y: 92 + 78, width: 300, height: 30))
         status.backgroundColor = .lightGray
         status.textColor = .gray
         status.font = UIFont.systemFont(ofSize: 14, weight: .regular)
@@ -53,7 +56,7 @@ class ProfileHeaderView: UIView {
     }()
     
     private lazy var statusButton: UIButton = {
-        let button = UIButton(frame: CGRect(x: 16, y: 92 + 112, width: 350, height: 50))
+        let button = UIButton(frame: CGRect(x: 16, y: 250, width: 350, height: 50))
         button.backgroundColor = .blue
         button.setTitleColor(.white, for: .normal)
         button.layer.cornerRadius = 4
@@ -61,14 +64,35 @@ class ProfileHeaderView: UIView {
         button.layer.shadowRadius = 4
         button.layer.shadowColor = UIColor.black.cgColor
         button.layer.shadowOpacity = 0.7
-        button.setTitle("Show status", for: .normal)
+        button.setTitle("Get status", for: .normal)
         button.addTarget(self, action: #selector(showStatus), for: .touchUpInside)
         
         return button
     }()
     
     @objc private func showStatus() {
+        statusView.text = statusText
         print(statusView.text ?? "No status")
+        
+    }
+    
+    private let statusChange: UITextField = {
+       let status = UITextField(frame: CGRect(x: 150, y: 200, width: 220, height: 40))
+        status.font = UIFont.systemFont(ofSize: 15, weight: .regular)
+        status.textColor = .black
+        status.layer.borderColor = UIColor.black.cgColor
+        status.layer.borderWidth = 1
+        status.layer.backgroundColor = UIColor.white.cgColor
+        status.layer.cornerRadius = 12
+        status.addTarget(self, action: #selector(statusTextChanged), for: .editingChanged)
+        
+        return status
+    }()
+    
+    @objc private func statusTextChanged(_ textField:UITextField) {
+        let text = textField.text ?? ""
+        statusText.removeAll()
+        statusText.append(text)
     }
     
     private var headerBack: UIView = {
@@ -76,17 +100,7 @@ class ProfileHeaderView: UIView {
         header.backgroundColor = .white
         
         return header
-    }()
-    
-    func addElements() {
-        self.addSubview(avatarView)
-        self.addSubview(headerView)
-        self.addSubview(statusView)
-        self.addSubview(statusButton)
-        self.addSubview(headerBack)
-    }
-    
-    
+    }()// не понял как полностью закрасить tab bar сверху, поэтому придумал такой костыль. Буду благодарен за подсказку
     
 }
     
